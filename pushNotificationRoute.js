9aa6b74b0f27bdd3834e16ad2dfdb2624f891c3b
Luis Sanchez <sanchezl391@gmail.com>
2018-10-28 15:38:29 -0600

    added app.use for cors

--- pushNotificationRoute.js
+++ pushNotificationRoute.js
@@ -3,6 +3,16 @@ const router = express.Router();
 var bodyParser = require('body-parser');
 const webpush = require('web-push');
 var app = express();
+// Session Middle ware
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(function(req, res, next) {
+    res.setHeader("Access-Control-Allow-Origin", '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.setHeader("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 app.use(bodyParser.json());
 
 
--- registerRoute.js
+++ registerRoute.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const time = require('./time');
 const config = require('./database');
-
-
 
 var bodyParser = require('body-parser');
 var app = express();
